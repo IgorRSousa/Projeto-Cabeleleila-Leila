a08f8173c2e388f7f74ef262ea0a5477d0d86e40
Igor Rafael Sousa <106490786+IgorRSousa@users.noreply.github.com>
2023-04-06 12:32:27 -0300

    Delete codigo directory

--- codigo/agendamento/editar-agenda.php
+++
@@ -1,8 +0,0 @@
-<h1>Editar agendamento</h1>
-<?php
-    $sql = "SELECT * FROM agendamentos WHERE id=".$_REQUEST["id"];
-    $res = $conn->query($sql);
-    $row = $res->fetch_object();
-
-    include("./includes/alterarAgendamento.php");
-?>
